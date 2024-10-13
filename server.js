db25d33141ba90a772fee3d31b071ba96bd9016f
Kevin <kevin.lin@leinotek.com>
2024-10-13 19:11:09 +0800

    測試問題

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 const port = process.env.PORT || 8080;
 
 const admin = require("firebase-admin");
-const serviceAccount = require("./secret/service-account-key.json");
+// const serviceAccount = require("./secret/service-account-key.json");
 
 require("./service/newest-service");
 
@@ -38,6 +38,6 @@ app.listen(port, () => {
 });
 
 //初始化 firebase admin
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
